38715d4a674e67816572dbec02a2eac07d8e81e6
GabriPoli <gfo2@poli.br>
2025-02-21 15:03:07 -0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
+st.title("SamurAI")
+
+uploaded_file = st.file_uploader("Escolha o arquivo")
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
