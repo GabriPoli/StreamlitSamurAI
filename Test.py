8a98814361d564a7612b389d98031fb18e4b17f1
GabriPoli <gfo2@poli.br>
2025-02-21 15:45:44 -0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -53,6 +53,6 @@ if uploaded_file is not None:
             # Exibir imagens lado a lado
             col1, col2 = st.columns(2)
             with col1:
-                st.image(image, caption="Original", use_column_width=True)
+                st.image(image, caption="Original", use_container_width=True)
             with col2:
-                st.image(edited_image, caption="Editada", use_column_width=True)
+                st.image(edited_image, caption="Editada", use_container_width=True)
